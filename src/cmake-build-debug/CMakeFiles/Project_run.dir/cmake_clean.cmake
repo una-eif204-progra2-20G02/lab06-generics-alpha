04af46340cccaf36d75f3ef4501918e355707537
computer <madrigalsteve@gmail.com>
2020-09-12 15:23:24 -0600

    Solved v3.00

---
+++ src/cmake-build-debug/CMakeFiles/Project_run.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_run.dir/CMakeFiles/3.17.3/CompilerIdCXX/CMakeCXXCompilerId.obj"
+  "CMakeFiles/Project_run.dir/main.obj"
+  "Project_run"
+  "Project_run.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/cmake-build-default/CMakeFiles/simple_example_lib.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_example_lib.dir/CMakeFiles/3.17.3/CompilerIdCXX/CMakeCXXCompilerId.cpp.obj"
+  "CMakeFiles/simple_example_lib.dir/cmake-build-debug/CMakeFiles/3.17.3/CompilerIdCXX/CMakeCXXCompilerId.cpp.obj"
+  "CMakeFiles/simple_example_lib.dir/main.cpp.obj"
+  "libsimple_example_lib.a"
+  "libsimple_example_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_example_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
