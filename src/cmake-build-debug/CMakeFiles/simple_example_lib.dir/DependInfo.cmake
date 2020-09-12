a2bf32ea43babadb83d233e0803fdd0a119caa58
computer <madrigalsteve@gmail.com>
2020-09-12 16:35:17 -0600

    final v1.00

---
+++ src/cmake-build-debug/CMakeFiles/simple_example_lib.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/Users/computer/CLionProjects/lab06-generics-alpha/src/cmake-build-debug/CMakeFiles/3.17.3/CompilerIdCXX/CMakeCXXCompilerId.cpp" "/cygdrive/c/Users/computer/CLionProjects/lab06-generics-alpha/src/cmake-build-debug/CMakeFiles/simple_example_lib.dir/CMakeFiles/3.17.3/CompilerIdCXX/CMakeCXXCompilerId.cpp.obj"
+  "/cygdrive/c/Users/computer/CLionProjects/lab06-generics-alpha/src/cmake-build-default/CMakeFiles/3.17.3/CompilerIdCXX/CMakeCXXCompilerId.cpp" "/cygdrive/c/Users/computer/CLionProjects/lab06-generics-alpha/src/cmake-build-debug/CMakeFiles/simple_example_lib.dir/cmake-build-default/CMakeFiles/3.17.3/CompilerIdCXX/CMakeCXXCompilerId.cpp.obj"
+  "/cygdrive/c/Users/computer/CLionProjects/lab06-generics-alpha/src/main.cpp" "/cygdrive/c/Users/computer/CLionProjects/lab06-generics-alpha/src/cmake-build-debug/CMakeFiles/simple_example_lib.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- tst/PersonTest.cpp
+++ tst/PersonTest.cpp
@@ -9,9 +9,9 @@
 #include "gtest/gtest.h"
 
 TEST(PersonTestSuite, VerifyProcessPayment){
-    Person person;
+    Person<std::string>* person=new Person<std::string>();
 
-    EXPECT_EQ(person.processPaymentBankTransfer(), "Sending the money by transference");
-    EXPECT_EQ(person.processPaymentCash(), "Give the money in the hands");
-    EXPECT_EQ(person.processPaymentCheck(), "Sending the check with the money");
+    EXPECT_EQ(person->processPaymentBankTransfer(), "Sending the money by transference");
+    EXPECT_EQ(person->processPaymentCash(), "Give the money in the hands");
+    EXPECT_EQ(person->processPaymentCheck(), "Sending the check with the money");
 }
