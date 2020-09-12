2aefd2ce659032e23832bfdb27be8802c368399f
computer <madrigalsteve@gmail.com>
2020-09-12 17:25:55 -0600

    Final Solve v.100

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,19 @@ int main()
 {
     std::cout << "Hello, Universidad Nacional!" << std::endl;
     //Pointer to class set Attributes
-    Person<std::string>* _person0=new Person<std::string>("Alvino","Vargas","123453354");
-    //Calling string method
+    Person<std::string>* _person0=new Person<std::string>();
+    //Pointer to set methods set Attributes
+    _person0->setFirstName("Claus");
+    _person0->setLastName("Hilden");
+    _person0->setDocumentId("4-2345-6435");
+    //Calling string metho
     std::cout<<_person0->toString();
     //Type of transference, pointer to method
     std::cout<<_person0->processPaymentBankTransfer()<<"\n";
     //Pointer to set methods set Attributes
     _person0->setFirstName("Johan");
     _person0->setLastName("Sebastian");
-    _person0->setDocumentId("12345678");
+    _person0->setDocumentId("1-2345-9678");
     std::cout<<"\n";
     //Calling string method
     std::cout<<_person0->toString();
@@ -25,7 +29,7 @@ int main()
     //Pointer to set methods set Attributes
     _person0->setFirstName("Mariano");
     _person0->setLastName("Figueroa");
-    _person0->setDocumentId("456789890");
+    _person0->setDocumentId("4-5678-9890");
     std::cout<<"\n";
     //Calling string method
     std::cout<<_person0->toString();
